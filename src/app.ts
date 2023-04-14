5f6ae88ae3593d973f8fb12c65c9a330d4b4dfa9
Bruna Terribile <brunaterribile@gmail.com>
2023-04-13 22:10:40 -0300

    refactor: routes in use, next in error file

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ app
   .use('/event', eventsRouter)
   .use('/enrollments', enrollmentsRouter)
   .use('/tickets', ticketsRouter)
-  .use('/payments')
+  //.use('/payments')
   .use(handleApplicationErrors);
 
 export function init(): Promise<Express> {
